a9277e12fb909e5c6b18ccd7bbb5c896688fa710
Ahmed Elemam <ahmed.elemam@its.ws>
2018-07-16 10:23:29 +0200

    secure login

--- DRM/DRM-web/src/main/java/Login/PaymentController.java
+++ DRM/DRM-web/src/main/java/Login/PaymentController.java
@@ -6,6 +6,8 @@
 package Login;
 
 
+import java.security.SecureRandom;
+import java.util.Arrays;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,6 +33,18 @@ public class PaymentController {
  private static final int AUTH_FAILURE = 102;
  
  
+ public static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+// 2048 bit keys should be secure until 2030 - https://web.archive.org/web/20170417095741/https://www.emc.com/emc-plus/rsa-labs/historical/twirl-and-rsa-key-size.htm
+public static final int SECURE_TOKEN_LENGTH = 256;
+
+private static final SecureRandom random = new SecureRandom();
+
+private static final char[] symbols = CHARACTERS.toCharArray();
+
+private static final char[] buf = new char[SECURE_TOKEN_LENGTH];
+ 
+ 
  
  @RequestMapping(value = "/Login", method = RequestMethod.POST)
  public Login_ouput pay(@RequestParam(value = "key") String key, @RequestBody Login_CR request) {
@@ -43,13 +57,25 @@ public class PaymentController {
   String keys = request.getKey();
    // Process the request
    // ....
-   // Return success response to the client.
-   response.setTokean(SUCCESS_STATUS);
+
+
+   
+   
+   response.setTokean(nextToken());
    response.setCode(CODE_SUCCESS);
   } else {
    response.setTokean(ERROR_STATUS);
    response.setCode(AUTH_FAILURE);
   }
+  
+     // Return success response to the client.
   return response;
  }
+ 
+ 
+ public static String nextToken() {
+    for (int idx = 0; idx < buf.length; ++idx)
+        buf[idx] = symbols[random.nextInt(symbols.length)];
+    return new String(buf);
+}
 }
